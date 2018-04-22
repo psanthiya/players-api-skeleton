fca3eeff9b2306c36ba4e9c4c0df211c8785bf4d
poorna <poorna.santhiya@rackspace.com>
2018-04-21 23:57:35 -0500

    Cleaned up UI related files and code

--- src/server.js
+++ src/server.js
@@ -9,65 +9,25 @@ var db = require('./config/db');
 var expressValidator     = require('express-validator');
 var bodyParser = require('body-parser');
 var http = require('http').Server(app);
-server.listen(port, function() {
-  console.log('Express server listening on port ' + port);
-});
-
-
 
 var userRoute = require('./routes/userRoute');
 var playerRoute = require('./routes/playerRoute');
 var matchRoute = require('./routes/matchRoute');
-//userRoute(app);
 
-
-app.use(function(req, res, next) {
-  if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT') {
-    jsonwebtoken.verify(req.headers.authorization.split(' ')[1], 'RESTFULAPIs', function(err, decode) {
-      if (err) req.user = undefined;
-      req.user = decode;
-      next();
-    });
-  } else {
-    req.user = undefined;
-    next();
-  }
-});
 app.use(bodyParser.json({ type: 'application/json' }));
 app.use(bodyParser.urlencoded({ extended: false }))
 
-// parse application/json
 app.use(bodyParser.json())
 app.use(expressValidator());
-console.log(__dirname);
-app.use('/static', express.static( './src/static'));
-app.use(express.static(__dirname + '/public'));
 
-app.set('views', './src/views');
-app.set('view engine', 'ejs');
-//app.set('view engine', 'html')
+
 app.use('/api', userRoute);
 app.use('/api', playerRoute);
 app.use('/api', matchRoute);
-//app.use('/api', matchRoute);
 
-//app.use('/api', playerRoute);
-
-//app.use('/api/user', userRoute);
-//app.use('/api/login', userRoute);
-
-app.use(function(req, res,next) {
-console.log("**************HERE"+req.headers);
-next();
+server.listen(port, function() {
+  console.log('Server starts on port ' + port);
 });
 
 
-
 module.exports = app;
-
-/*
-module.exports = {
-  Player: {},
-  User: {}
-};
-*/
